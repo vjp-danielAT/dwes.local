5b43bc4b27e8687d84e9469872b280702bf5907f
Dani <dalcont01@informatica.iesvalledeljerteplasencia.es>
2024-11-07 19:15:43 +0100

    A medias práctica 15

--- index.php
+++ index.php
@@ -3,13 +3,8 @@
 require 'utils/utils.php';
 require_once 'entity/imagenGaleria.class.php';
 require_once 'entity/partner.class.php';
-
-$imagenes = [];
-
-// Crea un array de objetos ImagenGaleria y les asigna valores por defecto
-for ($i = 1; $i <= 12; $i++) {
-    $imagenes[] = new ImagenGaleria($i . '.jpg', 'descripción imagen ' . $i);
-}
+require_once 'entity/imagenGaleriaRepository.class.php';
+require_once 'entity/connection.class.php';
 
 // Array de asociados
 $nombresAsociados = ['Mario', 'Hugo', 'Jose', 'Adri', 'Iker'];
@@ -20,5 +15,24 @@ for ($i = 0; $i < sizeof($nombresAsociados); $i++) {
 }
 
 $tresAsociados = extraer3Aleatorios($asociados);
+
+// Imágenes de la galería
+
+try {
+
+	// Crea una conexión con la BBDD
+	$config = require_once 'utils/config.php';
+	App::bind('config', $config);
+	$connection = App::getConnection();
+
+    /* ImagenRepository, usada para realizar operaciones
+	INSERT y SELECT con la BBDD */
+	$imagenRepository = new ImagenGaleriaRepository();
+
+} catch (QueryException | AppException $exc) {
+	$error = $exc->getMessage();
+} finally {
+	$imagenes = $imagenRepository->findAll();
+}
 
 require 'views/index.view.php';
