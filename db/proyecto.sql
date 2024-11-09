d7685c0be61599b10e5ce2cd705a077067cdb139
Dani <dalcont01@informatica.iesvalledeljerteplasencia.es>
2024-11-09 23:30:59 +0100

    Añadida copia de la BBDD e imágenes para la BBDD

--- db/proyecto.sql
+++ db/proyecto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2024 a las 19:02:49
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 09-11-2024 a las 23:27:08
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `asociados` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `logo` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `descripcion` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL
+  `nombre` varchar(255) NOT NULL,
+  `logo` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
@@ -42,7 +42,7 @@ CREATE TABLE `asociados` (
 
 CREATE TABLE `categorias` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(255) NOT NULL,
   `numImagenes` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -51,9 +51,9 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `numImagenes`) VALUES
-(1, 'Categoría 1', 0),
-(2, 'Categoría 2', 0),
-(3, 'Categoría 3', 0);
+(1, 'Categoría 1', 12),
+(2, 'Categoría 2', 6),
+(3, 'Categoría 3', 6);
 
 -- --------------------------------------------------------
 
@@ -63,13 +63,31 @@ INSERT INTO `categorias` (`id`, `nombre`, `numImagenes`) VALUES
 
 CREATE TABLE `imagenes` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `descripcion` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
   `numVisualizaciones` int(11) NOT NULL DEFAULT 0,
   `numLikes` int(11) NOT NULL DEFAULT 0,
   `numDownloads` int(11) NOT NULL DEFAULT 0,
   `categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `imagenes`
+--
+
+INSERT INTO `imagenes` (`id`, `nombre`, `descripcion`, `numVisualizaciones`, `numLikes`, `numDownloads`, `categoria`) VALUES
+(1, 'conejo1.jpg', '', 885, 431, 168, 1),
+(2, 'conejo2.jpg', '', 778, 347, 59, 1),
+(3, 'conejo3.jpg', '', 958, 333, 187, 1),
+(4, 'conejo4.png', '', 734, 388, 98, 1),
+(5, 'gato1.png', '', 868, 318, 106, 2),
+(6, 'gato2.jpg', '', 996, 281, 149, 2),
+(7, 'gato3.jpg', '', 562, 394, 116, 2),
+(8, 'gato4.jpg', '', 683, 301, 95, 2),
+(9, 'perro1.jpg', '', 755, 394, 124, 3),
+(10, 'perro2.jpg', '', 769, 412, 113, 3),
+(11, 'perro3.jpg', '', 527, 459, 96, 3),
+(12, 'perro4.jpg', '', 596, 411, 155, 3);
 
 -- --------------------------------------------------------
 
@@ -79,11 +97,11 @@ CREATE TABLE `imagenes` (
 
 CREATE TABLE `mensajes` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `apellidos` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
-  `asunto` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
-  `texto` text COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `asunto` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `texto` text DEFAULT NULL,
   `fecha` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
@@ -136,7 +154,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `mensajes`
