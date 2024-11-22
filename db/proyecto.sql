2616f81bf26f79d791b5c15b7aa8428c5bb0e6f7
Dani <dalcont01@informatica.iesvalledeljerteplasencia.es>
2024-11-22 14:01:20 +0100

    Proyecto 1EV

--- db/proyecto.sql
+++ db/proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-11-2024 a las 01:52:32
+-- Tiempo de generación: 22-11-2024 a las 13:59:41
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -39,11 +39,11 @@ CREATE TABLE `asociados` (
 --
 
 INSERT INTO `asociados` (`id`, `nombre`, `logo`, `descripcion`) VALUES
-(1, 'Adri', 'adri.jpg', 'Adri mi asociado'),
-(2, 'Mario', 'mario.jpg', ''),
-(3, 'Iker', 'iker.jpg', ''),
+(1, 'Iker', 'iker.jpg', ''),
+(2, 'Adri', 'adri.jpg', ''),
+(3, 'Hugo', 'hugo.jpg', ''),
 (4, 'Jose', 'jose.jpg', ''),
-(5, 'Hugo', 'hugo.jpg', '');
+(5, 'Mario', 'mario.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -62,9 +62,9 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `numImagenes`) VALUES
-(1, 'Categoría 1', 0),
-(2, 'Categoría 2', 0),
-(3, 'Categoría 3', 0);
+(1, 'Categoría 1', 10),
+(2, 'Categoría 2', 1),
+(3, 'Categoría 3', 1);
 
 -- --------------------------------------------------------
 
@@ -81,6 +81,24 @@ CREATE TABLE `imagenes` (
   `numDownloads` int(11) NOT NULL DEFAULT 0,
   `categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `imagenes`
+--
+
+INSERT INTO `imagenes` (`id`, `nombre`, `descripcion`, `numVisualizaciones`, `numLikes`, `numDownloads`, `categoria`) VALUES
+(1, 'conejo1.jpg', 'Conejo 1', 860, 279, 106, 1),
+(2, 'conejo2.jpg', '', 677, 391, 114, 1),
+(3, 'conejo3.jpg', '', 722, 388, 99, 3),
+(4, 'conejo4.png', '', 509, 350, 170, 2),
+(5, 'gato1.png', '', 991, 443, 64, 1),
+(6, 'gato2.jpg', '', 577, 255, 119, 1),
+(7, 'gato3.jpg', '', 597, 441, 149, 1),
+(8, 'gato4.jpg', '', 798, 329, 60, 1),
+(9, 'perro1.jpg', '', 714, 474, 160, 1),
+(10, 'perro2.jpg', '', 952, 471, 179, 1),
+(11, 'perro3.jpg', '', 914, 472, 100, 1),
+(12, 'perro4.jpg', '', 560, 288, 63, 1);
 
 -- --------------------------------------------------------
 
@@ -103,7 +121,7 @@ CREATE TABLE `mensajes` (
 --
 
 INSERT INTO `mensajes` (`id`, `nombre`, `apellidos`, `asunto`, `email`, `texto`, `fecha`) VALUES
-(1, 'Daniel', 'Alcon', 'Prueba nuevo mensaje', 'dani@gmail.com', 'Holaaaaaaaaaaaa', '2024-11-13 19:50:21');
+(1, 'Daniel', 'Alcon', 'Prueba nuevo mensaje', 'dani@gmail.com', 'Hola', '2024-11-22 13:56:04');
 
 --
 -- Índices para tablas volcadas
@@ -154,7 +172,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `mensajes`
