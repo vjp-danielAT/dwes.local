527dbefaabb06cf5f702e6defd096525cabb449c
Dani <dalcont01@informatica.iesvalledeljerteplasencia.es>
2024-12-10 19:31:56 +0100

    BD actualizada

--- db/proyecto.sql
+++ db/proyecto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-11-2024 a las 13:59:41
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Tiempo de generación: 10-12-2024 a las 19:30:49
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,21 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `asociados` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `logo` varchar(255) NOT NULL,
-  `descripcion` varchar(255) NOT NULL
+  `nombre` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `logo` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
---
--- Volcado de datos para la tabla `asociados`
---
-
-INSERT INTO `asociados` (`id`, `nombre`, `logo`, `descripcion`) VALUES
-(1, 'Iker', 'iker.jpg', ''),
-(2, 'Adri', 'adri.jpg', ''),
-(3, 'Hugo', 'hugo.jpg', ''),
-(4, 'Jose', 'jose.jpg', ''),
-(5, 'Mario', 'mario.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -53,7 +42,7 @@ INSERT INTO `asociados` (`id`, `nombre`, `logo`, `descripcion`) VALUES
 
 CREATE TABLE `categorias` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
+  `nombre` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
   `numImagenes` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -62,9 +51,9 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `numImagenes`) VALUES
-(1, 'Categoría 1', 10),
-(2, 'Categoría 2', 1),
-(3, 'Categoría 3', 1);
+(1, 'Categoría 1', 0),
+(2, 'Categoría 2', 0),
+(3, 'Categoría 3', 0);
 
 -- --------------------------------------------------------
 
@@ -74,31 +63,13 @@ INSERT INTO `categorias` (`id`, `nombre`, `numImagenes`) VALUES
 
 CREATE TABLE `imagenes` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `descripcion` varchar(255) NOT NULL,
+  `nombre` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
   `numVisualizaciones` int(11) NOT NULL DEFAULT 0,
   `numLikes` int(11) NOT NULL DEFAULT 0,
   `numDownloads` int(11) NOT NULL DEFAULT 0,
   `categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
---
--- Volcado de datos para la tabla `imagenes`
---
-
-INSERT INTO `imagenes` (`id`, `nombre`, `descripcion`, `numVisualizaciones`, `numLikes`, `numDownloads`, `categoria`) VALUES
-(1, 'conejo1.jpg', 'Conejo 1', 860, 279, 106, 1),
-(2, 'conejo2.jpg', '', 677, 391, 114, 1),
-(3, 'conejo3.jpg', '', 722, 388, 99, 3),
-(4, 'conejo4.png', '', 509, 350, 170, 2),
-(5, 'gato1.png', '', 991, 443, 64, 1),
-(6, 'gato2.jpg', '', 577, 255, 119, 1),
-(7, 'gato3.jpg', '', 597, 441, 149, 1),
-(8, 'gato4.jpg', '', 798, 329, 60, 1),
-(9, 'perro1.jpg', '', 714, 474, 160, 1),
-(10, 'perro2.jpg', '', 952, 471, 179, 1),
-(11, 'perro3.jpg', '', 914, 472, 100, 1),
-(12, 'perro4.jpg', '', 560, 288, 63, 1);
 
 -- --------------------------------------------------------
 
@@ -108,20 +79,13 @@ INSERT INTO `imagenes` (`id`, `nombre`, `descripcion`, `numVisualizaciones`, `nu
 
 CREATE TABLE `mensajes` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `apellidos` varchar(255) DEFAULT NULL,
-  `asunto` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `texto` text DEFAULT NULL,
+  `nombre` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellidos` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `asunto` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `texto` text COLLATE utf8mb4_spanish_ci DEFAULT NULL,
   `fecha` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
---
--- Volcado de datos para la tabla `mensajes`
---
-
-INSERT INTO `mensajes` (`id`, `nombre`, `apellidos`, `asunto`, `email`, `texto`, `fecha`) VALUES
-(1, 'Daniel', 'Alcon', 'Prueba nuevo mensaje', 'dani@gmail.com', 'Hola', '2024-11-22 13:56:04');
 
 --
 -- Índices para tablas volcadas
@@ -160,7 +124,7 @@ ALTER TABLE `mensajes`
 -- AUTO_INCREMENT de la tabla `asociados`
 --
 ALTER TABLE `asociados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
@@ -172,13 +136,13 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `mensajes`
 --
 ALTER TABLE `mensajes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
